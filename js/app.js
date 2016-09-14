b9a65ba95b10a920db1ced93dac93caef7642151
Karen Langner <krenabeth@yahoo.com>
2016-09-14 14:13:09 -0400

    styling nav and title, added logo

--- js/app.js
+++ js/app.js
@@ -1,1 +1,7 @@
 var app = angular.module('plumberApp', ['ngRoute']);
+
+app.controller("navController",["$scope", "$location", function($scope, $location){
+  $scope.isActive = function(destination){
+    return destination === $location.path();
+  }
+}]);
